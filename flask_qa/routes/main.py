8103f37dd8a2b6e032a30127d28cb059a18e20f1
Aqdas <aqdasak@gmail.com>
2020-07-19 11:41:25 +0530

    a

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -8,7 +8,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html', **context)
+    return render_template('index.html',)
 
 
 @main.route("/about")
