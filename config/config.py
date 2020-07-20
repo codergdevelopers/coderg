245e77385dd1c2b29d457b8eefcae30fbbef52c3
Aqdas <aqdasak@gmail.com>
2020-07-20 11:42:40 +0530

    Config.py working correctly

--- config/config.py
+++ config/config.py
@@ -3,8 +3,7 @@ params = {
     "tag_line": "Never Stay Still!!",
     "about_text": {
         "0": "We at CODERG try to explore the different dimensions of this coding world by reaching more number of peoples from different colleges of our country sharing their skills with all of us.",
-        "1": "I would like to thank AQDAS,The Chief developer of CODERG and my dearest friend for his support and his always helping gesture.",
-        "2": "This is demo"
+        "1": "I would like to thank AQDAS,The Chief developer of CODERG and my dearest friend for his support and his always helping gesture."
     },
     "project_categories": {
         "0": "python",
