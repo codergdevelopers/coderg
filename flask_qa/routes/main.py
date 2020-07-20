754505de91b97cd2c04d90fafabfd073475b6971
Aqdas <aqdasak@gmail.com>
2020-07-20 22:58:32 +0530

    Adding projects into database

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -45,36 +45,40 @@ def blog():
 #      THIS IS TO ADD PROJECTS IN DATABASE
 #      SHOULD BE RUN ONLY ONE TIME ON THE WEBSITE
 #
-#
-# @main.route("/addprojects")
-# def add_projects():
-#
-#     proj = Projects(category="Python", title="Hey soldier prettify my folder", language='Python', purpose='Batch rename files in a given folder. Can ignore files', working_on="Using wildcards in ignore list", link=r'https://github.com/aqdasak/Batch-Rename', author='Aqdas Ahmad Khan')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     proj = Projects(category="Java", title="Starke", language='Java', purpose='Greeting message on startup', working_on="Logic based printing of characters", link=r'https://github.com/deepanshdubey/starke', author='Aqdas Ahmad Khan (+Deepansh Dubey)')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     proj = Projects(category="Java", title="MorseCode_Decoder", language='Java', purpose='Decoding the characters of Morse Code', working_on="Identifying user inputs", link=r'https://github.com/deepanshdubey/MorseCode_Decoder', author='Deepansh Dubey')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     proj = Projects(category="WEB - D", title="Coderg Frontend", language='HTML, CSS & JS', purpose='Building a website for our society', working_on="HTML, CSS & JS", link=r'https://github.com/deepanshdubey/Coderg', author='Deepansh Dubey')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     proj = Projects(category="WEB - D", title="Coderg Backend", language='Python (flask)',
-#                     purpose='Backend of this website', working_on="Blogs section",
-#                     link=r'https://github.com/codergdevelopers/coderg', author='Aqdas Ahmad Khan')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     proj = Projects(category="WEB - D", title="React.calc", language='Javascript', purpose='Basic four function calculator', working_on="ReactJS", link=r'https://github.com/deepanshdubey/react.calc.git', author='Deepansh Dubey')
-#     db.session.add(proj)
-#     db.session.commit()
-#
-#     return "New post added"
-#
-#
+
+@main.route("/addprojects")
+def add_projects():
+    proj = Projects(category="Python", title="Healthy programmer.", language='Python', purpose="Reminder for resting eyes, drink water and do some exercise at regular interval of time. Have pause feature", working_on="Advanced pause and snooze", link=r'https://github.com/aqdasak/Healthy-Programmer', author='Aqdas Ahmad Khan')
+    db.session.add(proj)
+    db.session.commit()
+
+
+    proj = Projects(category="Python", title="Hey soldier prettify my folder", language='Python', purpose='Batch rename files in a given folder. Can ignore files', working_on="Using wildcards in ignore list", link=r'https://github.com/aqdasak/Batch-Rename', author='Aqdas Ahmad Khan')
+    db.session.add(proj)
+    db.session.commit()
+
+    proj = Projects(category="Java", title="Starke", language='Java', purpose='Greeting message on startup', working_on="Logic based printing of characters", link=r'https://github.com/deepanshdubey/starke', author='Aqdas Ahmad Khan (+Deepansh Dubey)')
+    db.session.add(proj)
+    db.session.commit()
+
+    proj = Projects(category="Java", title="MorseCode_Decoder", language='Java', purpose='Decoding the characters of Morse Code', working_on="Identifying user inputs", link=r'https://github.com/deepanshdubey/MorseCode_Decoder', author='Deepansh Dubey')
+    db.session.add(proj)
+    db.session.commit()
+
+    proj = Projects(category="WEB - D", title="Coderg Frontend", language='HTML, CSS & JS', purpose='Building a website for our society', working_on="HTML, CSS & JS", link=r'https://github.com/deepanshdubey/Coderg', author='Deepansh Dubey')
+    db.session.add(proj)
+    db.session.commit()
+
+    proj = Projects(category="WEB - D", title="Coderg Backend", language='Python (flask)',
+                    purpose='Backend of this website', working_on="Blogs section",
+                    link=r'https://github.com/codergdevelopers/coderg', author='Aqdas Ahmad Khan')
+    db.session.add(proj)
+    db.session.commit()
+
+    proj = Projects(category="WEB - D", title="React.calc", language='Javascript', purpose='Basic four function calculator', working_on="ReactJS", link=r'https://github.com/deepanshdubey/react.calc.git', author='Deepansh Dubey')
+    db.session.add(proj)
+    db.session.commit()
+
+    return "New post added"
+
+
