76c0980bb8694555ad3f6d3488eae47f8896e8d7
Aqdas <aqdasak@gmail.com>
2020-08-07 18:22:57 +0530

    relationship in data models

--- flask_qa/models.py
+++ flask_qa/models.py
@@ -4,15 +4,18 @@ from .extensions import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(), nullable=False)
 
-    username = db.Column(db.String(), primary_key=True, unique=True, nullable=False)
-    email = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(), unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
     _hashed_password = db.Column(db.String(255), nullable=False, server_default='')
 
-    admin = db.Column(db.Boolean, default=False)
-    editor = db.Column(db.Boolean, default=False)
+    role = db.relationship('Role', backref='user')
+    post = db.relationship('Post', backref='author')
+
+    # admin = db.Column(db.Boolean, default=False)
+    # editor = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -23,23 +26,21 @@ class User(db.Model):
         self._hashed_password = generate_password_hash(unhashed_password)
 
 
-class Project(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(), unique=False, nullable=False)
-    title = db.Column(db.String(), unique=True, nullable=False)
-    language = db.Column(db.String(), unique=False, nullable=False)
-    purpose = db.Column(db.String(), unique=False, nullable=False)
-    working_on = db.Column(db.String(), unique=False, nullable=False)
-    link = db.Column(db.String(), unique=False, nullable=False)
-    author = db.Column(db.String(), unique=False, nullable=False)
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class Post(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), nullable=False)
     tagline = db.Column(db.String(), nullable=False)
-    username = db.Column(db.String(), nullable=False)
-    fullname = db.Column(db.String(), nullable=False)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # username = db.Column(db.String(), nullable=False)
+    # fullname = db.Column(db.String(), nullable=False)
     slug = db.Column(db.String(), unique=True, nullable=False)
     content = db.Column(db.String(), nullable=False)
     date = db.Column(db.String(), nullable=True)
@@ -47,3 +48,13 @@ class Post(db.Model):
 # author is the username of the user
 # name should be fetched from User
 
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(), unique=False, nullable=False)
+    title = db.Column(db.String(), unique=True, nullable=False)
+    language = db.Column(db.String(), unique=False, nullable=False)
+    purpose = db.Column(db.String(), unique=False, nullable=False)
+    working_on = db.Column(db.String(), unique=False, nullable=False)
+    link = db.Column(db.String(), unique=False, nullable=False)
+    author = db.Column(db.String(), unique=False, nullable=False)
