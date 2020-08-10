39520b2c884209030d2f73f49bd04fa7623d17cd
Aqdas <aqdasak@gmail.com>
2020-08-10 20:54:45 +0530

    getting things right after reverting
    Some important changes were also reverted. Applied those important changes again.

--- coderg/util/verify.py
+++ coderg/util/verify.py
@@ -27,3 +27,18 @@ def check_role(*args):
                 return False
 
         return True
+
+
+# def role_required(*args, redirect_to='/'):
+#     def inner(func):
+#         def wrapper():
+#             from flask import redirect
+#
+#             if check_role(args):
+#                 func()
+#             else:
+#                 return redirect(redirect_to)
+#
+#         return wrapper
+#
+#     return inner
