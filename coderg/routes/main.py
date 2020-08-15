a6d28b23a9ea5047b10ea3b267717eaa05680849
Aqdas <aqdasak@gmail.com>
2020-08-15 13:01:06 +0530

    Admin can change user roles from dashboard

--- coderg/routes/main.py
+++ coderg/routes/main.py
@@ -28,12 +28,12 @@ def dashboard():
     # admin already logged in
     if check_role('ADMIN'):
         posts = Post.query.all()
-        return render_template('dashboard.html', posts=posts)
+        return render_template('dashboard.html', posts=posts, admin=True)
 
     # user already logged in
     elif 'user' in session:
         posts = User.query.filter_by(username=session['user']).first().post
-        return render_template('dashboard.html', posts=posts)
+        return render_template('dashboard.html', posts=posts, admin=False)
 
     # if 'user' not in session:
     else:
